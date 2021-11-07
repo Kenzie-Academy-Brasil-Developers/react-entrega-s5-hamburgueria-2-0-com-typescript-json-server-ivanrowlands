53dbb4c112183174ffcdbc4840b4859c1d0cb572
Ivan <ivan.jump@globo.com>
2021-11-07 20:11:45 -0300

    refactor: refactor app.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import {BrowserRouter} from "react-router-dom"
+import {ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Providers from "./providers"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <ToastContainer theme="colored" autoClose={2500} />
+        <BrowserRouter>
+            <Providers>
+                <App />
+            </Providers>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+)
